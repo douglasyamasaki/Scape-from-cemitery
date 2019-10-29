073d0d51dca2f7b070023e20d284a31b922dc8f7
douglasyamasaki <31442450+douglasyamasaki@users.noreply.github.com>
2019-10-29 14:39:05 -0300

    Update Models.cpp

--- Models.cpp
+++ Models.cpp
@@ -19,3 +19,14 @@ ArcherModel::ArcherModel() {
 	JumpUp->loadFromFile("./Sprites/Skeleton/Jump Up.png");
 	JumpDown->loadFromFile("./Sprites/Skeleton/Jump Down.png");
 }
+
+ArrowModel* ArrowModel::getInstance() {
+	if (!instance)
+		instance = new ArrowModel();
+	return instance;
+}
+
+ArrowModel::ArrowModel() {
+	Arrow = new sf::texture();
+	Arrow->loadFromFile("./Sprites/Skeleton/Arrow.png");
+}
