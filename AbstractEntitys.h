e066c5a731b43a7be53110524078acb8eff0cb9e
douglasyamasaki <31442450+douglasyamasaki@users.noreply.github.com>
2019-10-29 14:21:42 -0300

    Update AbstractEntitys.h

--- AbstractEntitys.h
+++ AbstractEntitys.h
@@ -5,6 +5,7 @@ namespace AbstractEntitys {
 	class Entity : public sf::RectangleShape {
 	public:
 		Entity(sf::Vector2f size,sf::Vector2f pos) : sf::RectangleShape(size) {
+			setOrigin(size/2.0f);
 			setPosition(pos);
 		}
 
@@ -14,13 +15,18 @@ namespace AbstractEntitys {
 	public:
 		StaticEntity(sf::Vector2f size, sf::Vector2f pos) : Entity(size, pos), Collisor(size) { 
 			hitbox.setPosition(pos);
-		}
+		},
 	};
 	class DynamicEntity : public Entity, public Collisor {
 	protected:
 		float speed;
 	public:
-		DynamicEntity(sf::Vector2f size, sf::Vector2f pos, float speed, sf::Vector2f hitbox) : Entity(size, pos), Collisor(hitbox) { this->speed = speed; }
+		DynamicEntity(sf::Vector2f size, sf::Vector2f pos, float speed, sf::Vector2f hitbox) : Entity(size, pos), Collisor(hitbox) 
+		{ this->speed = speed; }
+		DynamicEntity(sf::Vector2f size, sf::Vector2f pos, float speed, sf::Vector2f hitbox,sf::Vector2f deslocamento) : Entity(size, pos), 
+		Collisor(hitbox,deslocamento,pos) 
+		{ this->speed = speed; }
+		
 		virtual void update(float deltat) = 0;
 	};
 }
