0b9dbc859a5c63a7785628dcb29c263abb0465c7
dutse-ark <33733806+dutse-ark@users.noreply.github.com>
2019-11-26 11:20:55 -0300

    Update Warlock.h

--- Warlock.h
+++ Warlock.h
@@ -25,6 +25,7 @@ public:
 	int getAttType() const { return attacktype; }
 	void onHit(sf::Vector2f direction);
 	Warlock ( sf::Vector2f pos,ProjectileList* ref);
+	~Warlock(){ delete(textures);}
 	void update(float deltat);
 	void setP1(Player* p1) { this->p1 = p1; }
 	void setP2(Player* p2) { this->p2 = p2; }
