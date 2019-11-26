05a6ced21bb6d931dbc25a83c67b657fda5d66c4
dutse-ark <33733806+dutse-ark@users.noreply.github.com>
2019-11-26 10:56:33 -0300

    Update LevelStateHandler.h

--- LevelStateHandler.h
+++ LevelStateHandler.h
@@ -21,6 +21,7 @@ private:
 	enum class SaveType;
 public:
 	LevelStateHandler();
+	~LevelStateHandler(){}
 	void save();
 	void setP1(Player* p1) { this->p1 = p1; }
 	void setP2(Player* p2) { this->p2 = p2; }
