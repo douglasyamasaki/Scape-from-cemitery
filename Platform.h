bee88bb96ac0dbb8e415c4c9770dd80f72cae355
dutse-ark <33733806+dutse-ark@users.noreply.github.com>
2019-11-26 11:08:39 -0300

    Update Platform.h

--- Platform.h
+++ Platform.h
@@ -8,6 +8,7 @@ private:
 	sf::Vector2f hbpos;
 public:
 	Platform(sf::Vector2f size, sf::Vector2f pos) : StaticEntity(size, pos), Body(size) { setTexture(nullptr); setOrigin(getSize() / 2.0f); hitbox.setPosition(pos); hbpos = hitbox.getPosition(); }
+	~Plataform(){}
 	void fix() { hitbox.setPosition(hbpos); setPosition(hitbox.getPosition()); }
 };
 
