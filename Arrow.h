d894de1464d5995a2b1b7cb8d58b899c0c1988b6
dutse-ark <33733806+dutse-ark@users.noreply.github.com>
2019-11-26 10:15:38 -0300

    Update Arrow.h

--- Arrow.h
+++ Arrow.h
@@ -16,6 +16,7 @@ private:
 public:
 	void setShooter(Player* shooter) { origem = shooter; }
 	Arrow(sf::Vector2f pos, sf::Vector2f speed,Player* pref);
+	~Arrow();
 	void update(float deltat);
 	const sf::Vector2f getVelocity() const { return velocity; }
 	Player* getShooter() { return origem; }
